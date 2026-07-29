6958f6c7da68501f673ee2e795e856bdf05128f7
shamm-ma <281799477+shamm-ma@users.noreply.github.com>
2026-07-29 06:47:28 +0500

    Delete python directory

--- python/test_setup.py
+++
@@ -1,7 +0,0 @@
-import pandas
-import sqlite3
-import sklearn
-import matplotlib
-import seaborn
-
-print("Environment is working")
--- python/top20.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-#find top 20 Airports by no. flights for NOAA weather data comparison
-df= pd.read_csv("data/raw/flightdata/flights_2025_jan_apr.csv", usecols=["ORIGIN"])
-
-print(df["ORIGIN"].value_counts().head(20))
-print(df["ORIGIN"].nunique())
